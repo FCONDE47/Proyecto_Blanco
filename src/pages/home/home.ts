2d8f4fe1fa4ec76454c924d362bc3f4e946d897b
fabian morales <fconde47@gmail.com>
2018-07-31 18:45:36 -0500

    publico

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,51 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, public alerCtrl: AlertController) {
   }
 
+doAlert() {
+  let alert = this.alerCtrl.create({
+    title: 'Hola Amigo!',
+    message: 'Your friend, Obi wan Kenobi, just approved your friend request!',
+    buttons: ['Ok']
+  });
+  alert.present()
+}
+doPrompt() {
+  let prompt = this.alerCtrl.create({
+    title: 'Recuperar contraseña',
+    message: "Ingrese el correo registrado",
+    inputs: [
+      {
+        name: 'title',
+        placeholder: 'Mail'
+      },
+    ],
+    buttons: [
+      {
+        text: 'Cancelar',
+        handler: data => {
+          console.log('Cancel clicked');
+        }
+      },
+      {
+        text: 'Recuperar',
+        handler: data => {
+          console.log (data)
+          this.doConfirm(data)
+        }
+      }
+    ]
+  });
+  prompt.present();
+}
+doConfirm(data) {
+  let alert = this.alerCtrl.create({
+    title: 'Verifique su correo!',
+    message: 'Sus credenciales fuero enviadas al correo: \n' + data.title,
+    buttons: ['Ok']
+  });
+  alert.present()
+}
 }
